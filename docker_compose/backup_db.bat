846f5bbf6e094919fde5476db4bf1c6f7c35ce1a
scx1332 <sieciech.czajka@golem.network>
2022-01-14 14:39:56 +0100

    removed script

--- docker_compose/backup_db.bat
+++
@@ -1,7 +0,0 @@
-docker-compose down
-docker-compose up -d mongodb_container
-docker exec gas_scanner_mongo mongodump
-docker cp gas_scanner_mongo:dump .
-docker exec gas_scanner_mongo /bin/bash -c "rm -rf ./dump"
-docker exec gas_scanner_mongo /bin/bash -c "ls"
-docker-compose up -d
