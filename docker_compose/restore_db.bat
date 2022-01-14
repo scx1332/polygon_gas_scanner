a39e2a50ad94289e0e9c76982064f3c27cc6917a
scx1332 <sieciech.czajka@golem.network>
2022-01-14 14:39:38 +0100

    Restore db script

--- docker_compose/restore_db.bat
+++
@@ -1,8 +0,0 @@
-docker-compose down
-docker-compose up -d mongodb_container
-docker exec gas_scanner_mongo /bin/bash -c "rm -rf ./dump"
-docker cp dump gas_scanner_mongo:dump
-docker exec gas_scanner_mongo mongorestore
-docker exec gas_scanner_mongo /bin/bash -c "rm -rf ./dump"
-docker-compose up -d
-
---
+++ docker_compose/restore_db.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import os
+import shutil
+import sys
+
+input_dir = sys.argv[1]
+
+if not os.path.exists("{}/dump".format(input_dir)):
+    print("Directory does not exists:" + " {}/dump".format(input_dir))
+    exit(1)
+
+
+def run_command(command):
+    print("Running command: {}".format(command))
+    os.system(command)
+
+run_command("docker-compose down")
+run_command("docker-compose up -d mongodb_container")
+run_command('docker exec gas_scanner_mongo /bin/bash -c "rm -rf ./dump"')
+run_command('docker cp dump gas_scanner_mongo:{}/dump'.format(input_dir))
+run_command('docker exec gas_scanner_mongo mongorestore')
+run_command('docker exec gas_scanner_mongo /bin/bash -c "rm -rf ./dump"')
+run_command('docker-compose up -d')
+
+
+
+
+
+
+
+
+
