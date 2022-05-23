7e025410c15ed8877f0b3b5b63f269c6e06b3ed6
scx1332 <sieciech.czajka@golem.network>
2022-05-23 23:40:44 +0200

    Playing with plotly charts

--- gas_scanner_front/src/pages/plotlychart.jsx
+++ gas_scanner_front/src/pages/plotlychart.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import dynamic from 'next/dynamic';
 
 import Layout from './common/Layout';
@@ -21,17 +21,55 @@ const plot_data = [
 {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]}
 ];
 
+const plot_data2 = [
+    {
+        x: [1, 2, 3, 5],
+        y: [2, 6, 3, 6],
+        type: 'scatter',
+        mode: 'lines+markers',
+        marker: {color: 'red'},
+    }
+];
+
 const Plotlychart = () => {
+    const [plotData, setPlotData] = useState(plot_data);
+    const [count, setCount] = useState(0);
+
     useEffect(() => {
         async function fetchMyAPI() {
-            let lastBLocks = 10;
+            let lastBLocks = 20000;
             const BACKEND_URL = "http://145.239.69.80:8899";
             const res = await fetch(`${BACKEND_URL}/polygon/block-info/last-blocks?block_count=${lastBLocks}`);
             let json_result = await res.json();
-            console.log(json_result);
+            //console.log(json_result);
+
+            let xarray = [];
+            let yarray = [];
+            for (var i = 0; i < json_result.length; i++){
+                let obj = json_result[i];
+                xarray.push(obj["blockTime"]);
+                //yarray.push(obj["baseFeePrice"]);
+                yarray.push(obj["minGas"]);
+            }
+            const plot_data2 = [
+                {
+                    x: xarray,
+                    y: yarray,
+                    type: 'bar',
+                    //mode: 'lines+markers',
+                    marker: {color: 'blue'},
+                }/*,
+                {type: 'bar', x: xarray, y: yarray}*/
+            ];
+            setPlotData(plot_data2);
         }
 
-        fetchMyAPI()
+        if (count == 0) {
+            fetchMyAPI();
+            setCount(1);
+
+        }
+
     });
 
     return (
@@ -39,8 +77,8 @@ const Plotlychart = () => {
             <Flex direction="column">
                 <Plot
 
-                    data={plot_data}
-                    layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+                    data={plotData}
+                    layout={ {width: 720, height: 640, title: 'A Fancy Plot'} }
                 />
 
             </Flex>
