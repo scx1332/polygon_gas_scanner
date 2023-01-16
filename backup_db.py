30ba22030f1cd9862dbfa6916d30157f3600e43c
scx1332 <sieciech.czajka@golem.network>
2023-01-16 14:12:05 +0100

    Updated scripts

--- backup_db.py
+++ backup_db.py
@@ -2,9 +2,10 @@ from datetime import datetime
 import os
 import shutil
 
+mongo_container_name = os.getenv("MONGO_CONTAINER_NAME")
+
 if os.path.exists("dump"):
     shutil.rmtree("dump")
-
 
 def run_command(command):
     print("Running command: {}".format(command))
@@ -13,10 +14,10 @@ def run_command(command):
 
 run_command("docker-compose down")
 run_command("docker-compose up -d mongo")
-run_command("docker exec gas_scanner_mongo mongodump")
-run_command("docker cp gas_scanner_mongo:dump .")
-run_command('docker exec gas_scanner_mongo /bin/bash -c "rm -rf ./dump"')
-run_command('docker exec gas_scanner_mongo /bin/bash -c "ls"')
+run_command(f"docker exec {mongo_container_name} mongodump")
+run_command(f"docker cp {mongo_container_name}:dump .")
+run_command(f'docker exec {mongo_container_name} /bin/bash -c "rm -rf ./dump"')
+run_command(f'docker exec {mongo_container_name} /bin/bash -c "ls"')
 
 backup_dir = "backupdb_" + datetime.today().strftime('%Y-%m-%d_%H-%M-%S')
 os.mkdir(backup_dir)
--- restore_db.py
+++ restore_db.py
@@ -1,9 +1,12 @@
-from datetime import datetime
 import os
-import shutil
 import sys
+from dotenv import load_dotenv
+
+load_dotenv()
 
 input_dir = sys.argv[1]
+
+mongo_container_name = os.getenv("MONGO_CONTAINER_NAME")
 
 if not os.path.exists("{}/dump".format(input_dir)):
     print("Directory does not exists:" + " {}/dump".format(input_dir))
@@ -17,10 +20,10 @@ def run_command(command):
 
 run_command("docker-compose down")
 run_command("docker-compose up -d mongo")
-run_command('docker exec gas_scanner_mongo /bin/bash -c "rm -rf /dump"')
-run_command('docker cp {}/dump gas_scanner_mongo:/dump'.format(input_dir))
-run_command('docker exec gas_scanner_mongo mongorestore')
-run_command('docker exec gas_scanner_mongo /bin/bash -c "rm -rf /dump"')
+run_command(f'docker exec {mongo_container_name} /bin/bash -c "rm -rf /dump"')
+run_command(f'docker cp {input_dir}/dump gas_scanner_mongo:/dump')
+run_command(f'docker exec {mongo_container_name} mongorestore')
+run_command(f'docker exec {mongo_container_name} /bin/bash -c "rm -rf /dump"')
 run_command('docker-compose up -d')
 
 
